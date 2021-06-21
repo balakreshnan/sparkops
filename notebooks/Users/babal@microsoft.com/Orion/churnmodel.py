88566ad9fc76189f16de493a12b3f473dbefd61c
Bala <bala_b@hotmail.com>
2021-06-21 14:20:30 -0500

    Update: README.md azure-pipelines.yml notebooks

--- notebooks/Users/babal@microsoft.com/Orion/churnmodel.py
+++ notebooks/Users/babal@microsoft.com/Orion/churnmodel.py
@@ -163,7 +163,57 @@ print(lr.explainParams())
 
 # COMMAND ----------
 
-
+from pyspark.mllib.tree import DecisionTree, DecisionTreeModel
+from pyspark.mllib.util import MLUtils
 
 # COMMAND ----------
 
+featuredf = FeeRatiossdf.drop("churned")
+
+# COMMAND ----------
+
+from pyspark.ml import Pipeline
+from pyspark.ml.classification import RandomForestClassifier
+from pyspark.ml.feature import IndexToString, StringIndexer, VectorIndexer
+from pyspark.ml.evaluation import MulticlassClassificationEvaluator
+
+# COMMAND ----------
+
+labelIndexer = StringIndexer(inputCol="churned", outputCol="indexedLabel").fit(FeeRatiossdf)
+
+# Automatically identify categorical features, and index them.
+# Set maxCategories so features with > 4 distinct values are treated as continuous.
+#featureIndexer =\
+#    VectorIndexer(inputCol="features", outputCol="indexedFeatures", maxCategories=4).fit(featuredf)
+
+# Split the data into training and test sets (30% held out for testing)
+(trainingData, testData) = FeeRatiossdf.randomSplit([0.7, 0.3])
+
+# Train a RandomForest model.
+rf = RandomForestClassifier(labelCol="indexedLabel", featuresCol="features", numTrees=10)
+
+# Convert indexed labels back to original labels.
+labelConverter = IndexToString(inputCol="prediction", outputCol="predictedLabel",
+                               labels=labelIndexer.labels)
+assembler = VectorAssembler(inputCols=features, outputCol="features")
+
+# Chain indexers and forest in a Pipeline
+pipeline = Pipeline(stages=[labelIndexer, assembler, rf, labelConverter])
+
+# Train model.  This also runs the indexers.
+model = pipeline.fit(trainingData)
+
+# Make predictions.
+predictions = model.transform(testData)
+
+# Select example rows to display.
+predictions.select("predictedLabel", "churned", "features").show(5)
+
+# Select (prediction, true label) and compute test error
+evaluator = MulticlassClassificationEvaluator(
+    labelCol="indexedLabel", predictionCol="prediction", metricName="accuracy")
+accuracy = evaluator.evaluate(predictions)
+print("Test Error = %g" % (1.0 - accuracy))
+
+rfModel = model.stages[2]
+print(rfModel)  # summary only
