b33c88711364397ca6ae778f1b640d50ada3a09b
Bala <bala_b@hotmail.com>
2021-06-21 16:24:04 -0500

    Update: README.md azure-pipelines.yml notebooks

--- notebooks/Users/babal@microsoft.com/Titanic/titanicmodel.py
+++ notebooks/Users/babal@microsoft.com/Titanic/titanicmodel.py
@@ -84,11 +84,28 @@ display(titanicds1)
 
 # COMMAND ----------
 
+from pyspark.ml.feature import StringIndexer
+
+featureindexer = StringIndexer(inputCol="Name,Sex,Ticket,Cabin,Embarked", outputCol="featureIndex")
+
+# COMMAND ----------
+
+display(titanicds1)
+
+# COMMAND ----------
+
 (trainingData, testData) = titanicds1.randomSplit([0.7, 0.3])
 
 # COMMAND ----------
 
+featurescol = ["PassengerId", "Survived", "Pclass", "Age", "SibSp", "Parch", "Fare"]
+
+# COMMAND ----------
+
 labelIndexer = StringIndexer(inputCol="Survived", outputCol="indexedLabel").fit(titanicds1)
+
+#featureIndexer =\
+#    VectorIndexer(inputCol="PassengerId, Pclass, Name, Sex, Age, SibSp, Parch, Ticket, Fare, Cabin, Embarked", outputCol="indexedFeatures").fit(titanicds1)
 
 
 (trainingData, testData) = titanicds1.randomSplit([0.7, 0.3])
@@ -100,6 +117,62 @@ rf = RandomForestClassifier(labelCol="indexedLabel", featuresCol="features", num
 labelConverter = IndexToString(inputCol="prediction", outputCol="predictedLabel",
                                labels=labelIndexer.labels)
 assembler = VectorAssembler(inputCols=features, outputCol="features")
+
+# Chain indexers and forest in a Pipeline
+pipeline = Pipeline(stages=[labelIndexer, assembler, rf, labelConverter])
+
+# Train model.  This also runs the indexers.
+model = pipeline.fit(trainingData)
+
+# Make predictions.
+predictions = model.transform(testData)
+
+# Select example rows to display.
+predictions.select("predictedLabel", "Survived", "features").show(5)
+
+# Select (prediction, true label) and compute test error
+evaluator = MulticlassClassificationEvaluator(
+    labelCol="indexedLabel", predictionCol="prediction", metricName="accuracy")
+accuracy = evaluator.evaluate(predictions)
+print("Accuracy = ", accuracy)
+print("Test Error = %g" % (1.0 - accuracy))
+
+rfModel = model.stages[2]
+print(rfModel)  # summary only
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+featurescol = ["PassengerId", "Pclass", "Age", "SibSp", "Parch", "Fare"]
+
+# COMMAND ----------
+
+(trainingData, testData) = titanicds1.randomSplit([0.7, 0.3])
+
+# COMMAND ----------
+
+labelIndexer = StringIndexer(inputCol="Survived", outputCol="indexedLabel").fit(titanicds1)
+
+#featureIndexer =\
+#    VectorIndexer(inputCol="PassengerId, Pclass, Name, Sex, Age, SibSp, Parch, Ticket, Fare, Cabin, Embarked", outputCol="indexedFeatures").fit(titanicds1)
+
+
+(trainingData, testData) = titanicds1.randomSplit([0.7, 0.3])
+
+# Train a RandomForest model.
+rf = RandomForestClassifier(labelCol="indexedLabel", featuresCol="features", numTrees=10)
+
+# Convert indexed labels back to original labels.
+labelConverter = IndexToString(inputCol="prediction", outputCol="predictedLabel",
+                               labels=labelIndexer.labels)
+assembler = VectorAssembler(inputCols=featurescol, outputCol="features")
 
 # Chain indexers and forest in a Pipeline
 pipeline = Pipeline(stages=[labelIndexer, assembler, rf, labelConverter])
